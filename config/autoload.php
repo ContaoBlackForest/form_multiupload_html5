15d5498066ebbe1394aeb2a4bbe1efa8d707e26c
Dominik Tomasi <d.tomasi@tomasi-media.de>
2014-01-29 11:11:03 +0100

    First Push

---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+    'tomasiMedia',
+));
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    'tomasiMedia\FormFileUploadMultiple'            => 'system/modules/multiupload_html5/classes/FormFileUploadMultiple.php',
+));
+
