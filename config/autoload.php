89599f5a773adf5263fa584e4d60e0ab0e141743
Sven Baumann <baumann.sv@gmail.com>
2015-03-20 10:24:24 +0100

    fix autoload.php and config.php 	path and namespaces errors

--- config/autoload.php
+++ config/autoload.php
@@ -17,7 +17,7 @@ ClassLoader::addNamespaces(
 ClassLoader::addClasses(
 	array
 	(
-		'tomasiMedia\FormFileUploadMultiple' => 'system/modules/multiupload_html5/classes/FormFileUploadMultiple.php',
+		'tomasiMedia\FormFileUploadMultiple' => 'system/modules/form_multiupload_html5/classes/FormFileUploadMultiple.php',
 	)
 );
 
