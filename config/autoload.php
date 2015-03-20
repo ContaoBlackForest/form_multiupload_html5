7035d088f84e1f7db7636e04187ea21eeb4fd05a
Sven Baumann <baumann.sv@gmail.com>
2015-03-20 10:19:34 +0100

    reformat code

--- config/autoload.php
+++ config/autoload.php
@@ -3,17 +3,21 @@
 /**
  * Register the namespaces
  */
-ClassLoader::addNamespaces(array
-(
-    'tomasiMedia',
-));
+ClassLoader::addNamespaces(
+	array
+	(
+		'tomasiMedia',
+	)
+);
 
 
 /**
  * Register the classes
  */
-ClassLoader::addClasses(array
-(
-    'tomasiMedia\FormFileUploadMultiple'            => 'system/modules/multiupload_html5/classes/FormFileUploadMultiple.php',
-));
+ClassLoader::addClasses(
+	array
+	(
+		'tomasiMedia\FormFileUploadMultiple' => 'system/modules/multiupload_html5/classes/FormFileUploadMultiple.php',
+	)
+);
 
